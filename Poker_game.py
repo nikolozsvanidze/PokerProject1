70a91a9bea7e6f57958b2657da80985e28fa2b1e
mac <nsvanidze.ieu2023@student.ie.edu>
2025-03-26 18:03:41 +0100

    finished poker game (dock string not added yet)

--- Poker_game.py
+++ Poker_game.py
@@ -1,3 +1,5 @@
+from operator import truediv
+
 from deck import Deck, Card
 
 class PokerHand:
@@ -22,16 +24,65 @@ class PokerHand:
                 return False
         return True
 
+    @property
+    def is_full_house(self):
+        return self.number_matches == 8
+
+
+    @property
+    def number_matches(self):
+        matches = 0
+        for i in range(len(self.cards)):
+            for j in range(len(self.cards)):
+                if i == j:
+                    continue
+                if self.cards[i].rank == self.cards[j].rank:
+                    matches += 1
+        return matches
+
+    @property
+    def is_pair(self):
+        if self.number_matches == 2:   #simple
+            return True
+        return False
+
+
+    @property
+    def is_two_pair(self):
+        return self.number_matches == 4 #more advanced
+
+    @property
+    def is_trips(self):
+        if self.number_matches == 6:
+            return True
+        return False
+
+    @property
+    def is_quads(self):
+        if self.number_matches == 12:
+            return True
+        return False
+
+    @property
+    def is_straight(self):
+        self.cards.sort()
+        distance = Card.RANKS.index(self.cards[4].rank) - \
+                   Card.RANKS.index(self.cards[0].rank)
+        return self.number_matches ==0 and distance ==4
+
+
+
 
 count = 0
-flush = 0
-while flush < 100:
+matches = 0
+while matches < 10:
     deck = Deck()
     deck.shuffle()
     hand = PokerHand(deck)
-    if hand.is_flush:
-       flush += 1
+    if hand.is_straight:
+        matches += 1
+        print(hand)
     count += 1
 
-print(f"probability of flush is {100*flush/count}")
+print(f"probability of full straight is {100*matches/count}")
 
